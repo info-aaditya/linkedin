1e10eda37311c8b2488183338954a6298b466c51
info-aaditya <gaaditya@gmail.com>
2021-05-07 02:03:59 +0545

    Added Home page for Linkedin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import Login from "./components/Login";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           </Route>
           <Route path = "/home">
             <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
